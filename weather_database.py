c33a7e7cb34c0ce28c8dcda7120abb6b16cbc816
ebuaa <axel.aube@viacesi.fr>
2024-01-24 10:01:28 +0100

    ajout fichier création database

---
+++ weather_database.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+# Établir une connexion à la base de données ou la créer si elle n'existe pas
+connection = sqlite3.connect("weather.db")
+
+cursor = connection.cursor()
+
+# Définir la table Utilisateurs
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Utilisateurs
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        identifiant TEXT NOT NULL
+        mot_de_passe TEXT NOT NULL
+    )
+''')
+
+# Définir la table Sonde
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Sonde
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        humidite INT NOT NULL,
+        temperature FLOAT NOT NULL
+        pression FLOAT NOT NULL
+        horodatage DATETIME NOT NULL
+        id_sonde
+    )
+''')
+
+# Définir la table Releves
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Releves
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        humidite INT NOT NULL,
+        temperature FLOAT NOT NULL
+        pression FLOAT NOT NULL
+        horodatage DATETIME NOT NULL
+        FOREIGN KEY(id_sonde) REFERENCES Sonde(id_sonde)
+    )
+''')
+
+# Valider les insertions dans la base de données
+connection.commit()
+
+# Fermer la connexion à la base de données
+connection.close()
